59dc22643999b3c137aa4c43aa78d6d8dbc72f9c
Rafael Luiz <rlg.santos123@gmail.com>
2025-04-09 17:35:28 -0300

    Modificações na telade login

--- SPRINT3/Login/php/login.php
+++ SPRINT3/Login/php/login.php
@@ -3,7 +3,7 @@ session_start();
 include '../../Conexao/php/conexao.php';
 
 $email = $_POST['email'] ?? '';
-$senha_digitada = $_POST['senha'] ?? '';
+$senha_digitada = hash('sha256', $_POST['senha']) ?? '';
 
 if (empty($email) || empty($senha_digitada)) {
     die("Preencha todos os campos obrigatórios.");
@@ -18,13 +18,14 @@ $resultado = $stmt->get_result();
 if ($resultado->num_rows === 1) {
     $usuario = $resultado->fetch_assoc();
 
-    if (password_verify($senha_digitada, $usuario['senha_hash'])) {
+    if ($senha_digitada === $usuario['senha_hash']) {
         $_SESSION['usuario'] = $usuario['nome'];
         $_SESSION['email'] = $usuario['email'];
-        header("Location: ../../Dashboard/html/dashboard.html");
+        header("Location: ../../telaRegistros/html/telaRegistros.html");
         exit;
     } else {
         echo "Senha incorreta.";
+        
     }
 } else {
     echo "Usuário não encontrado.";
